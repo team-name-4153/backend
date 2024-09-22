03bb73c065b01bdd8d0df6b883e5f45e9d86e6e4
luigi617 <luigiliu617@gmail.com>
2024-09-21 20:42:26 -0400

    hls video play

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,26 @@
+# Dockerfile
 FROM python:3.10
 
-EXPOSE 5001
+EXPOSE ${FLASK_RUN_PORT}
 
 WORKDIR /backend-app
 
+# Install dependencies
 RUN apt-get update && apt-get install -y \
     libgl1-mesa-glx \
-    libglib2.0-0
+    libglib2.0-0 \
+    ffmpeg
 
 RUN pip install --upgrade pip
 COPY requirements.txt /backend-app
 RUN pip install -r requirements.txt
 
+# Copy the entire app directory
 COPY . /backend-app
 
-CMD python app.py
+# Set environment variables for Flask
+ENV FLASK_APP=/backend-app/app.py
+ENV FLASK_ENV=${FLASK_ENV}
+
+# Use the flask command for development with reloading enabled
+CMD python ${FLASK_APP}
--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from importlib import import_module
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, send_from_directory
 import os
+import sys
 
 if os.environ.get('CAMERA'):
     Camera = import_module('camera_' + os.environ['CAMERA']).Camera
 else:
-    from camera import Camera
-    # from test_camera import Camera
+    from camera.camera import Camera
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
@@ -19,29 +19,33 @@ app = create_app()
 
 @app.route('/')
 def index():
-    """Video streaming home page."""
+    # return "ok yes"
     return render_template('index.html')
 
-
-def gen(camera):
-    """Video streaming generator function."""
-    yield b'--frame\r\n'
-    while True:
-        frame = camera.get_frame()
-        yield b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n--frame\r\n'
+@app.route('/videos/<path:filename>')
+def serve_hls(filename):
+    return send_from_directory('test_video', filename)
 
 
-@app.route('/video_feed')
-def video_feed():
-    """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(Camera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+# def gen(camera):
+#     """Video streaming generator function."""
+#     yield b'--frame\r\n'
+#     while True:
+#         frame = camera.get_frame()
+#         yield b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n--frame\r\n'
+
+
+# @app.route('/video_feed')
+# def video_feed():
+#     """Video streaming route. Put this in the src attribute of an img tag."""
+#     return Response(gen(Camera()),
+#                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == '__main__':
     
     app.run(
-        debug=app.config["DEBUG"],
-        host='0.0.0.0',
-        port=5001,
+        debug=bool(int(app.config["DEBUG"])),
+        host=os.environ.get("FLASK_RUN_HOST", "0.0.0.0"),
+        port=int(os.environ.get("FLASK_RUN_PORT", "5000")),
     )
