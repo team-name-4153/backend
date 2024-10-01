3dfc206dec107be43a0c7ce2dbb091630f096348
Ming-Wang-Columbia <mw3705@columbia.edu>
2024-10-01 03:04:41 -0400

    Modified app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-
-from flask import Flask, render_template, Response, send_from_directory
-from flask_socketio import SocketIO, emit
+import requests
+from flask import Flask, render_template, Response, send_from_directory, request
+from flask_socketio import SocketIO, emit, join_room
+from flask_cors import CORS
 import os
 import sys
 from video_utils.utils import start_streaming_thread, stop_streaming_thread
@@ -13,12 +14,17 @@ from video_utils.utils import start_streaming_thread, stop_streaming_thread
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
+users_in_room = {}
+rooms_sid = {}
+names_sid = {}
+
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('settings.py')
     return app
 
 app = create_app()
+CORS(app, resources={r"/*": {"origins": "*"}})
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route('/')
@@ -55,6 +61,84 @@ def handle_video_frame(data):
 #     """Video streaming route. Put this in the src attribute of an img tag."""
 #     return Response(gen(Camera()),
 #                     mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@socketio.on("connect")
+def on_connect():
+    sid = request.sid
+    print("New socket connected ", sid)
+
+
+@socketio.on("join-room")
+def on_join_room(data):
+    sid = request.sid
+    room_id = data["room_id"]
+    display_name = data["name"]
+
+    # register sid to the room
+    join_room(room_id)
+    rooms_sid[sid] = room_id
+    names_sid[sid] = display_name
+
+    # broadcast to others in the room
+    print("[{}] New member joined: {}<{}>".format(room_id, display_name, sid))
+    emit("user-connect", {"sid": sid, "name": display_name},
+         broadcast=True, include_self=False, room=room_id)
+
+    # add to user list maintained on server
+    if room_id not in users_in_room:
+        users_in_room[room_id] = [sid]
+        emit("user-list", {"my_id": sid})  # send own id only
+    else:
+        usrlist = {u_id: names_sid[u_id]
+                   for u_id in users_in_room[room_id]}
+        # send list of existing users to the new member
+        emit("user-list", {"list": usrlist, "my_id": sid})
+        # add new member to user list maintained on server
+        users_in_room[room_id].append(sid)
+
+    print("\nusers: ", users_in_room, "\n")
+
+
+@socketio.on("disconnect")
+def on_disconnect():
+    sid = request.sid
+    room_id = rooms_sid[sid]
+    display_name = names_sid[sid]
+
+    print("[{}] Member left: {}<{}>".format(room_id, display_name, sid))
+    emit("user-disconnect", {"sid": sid},
+         broadcast=True, include_self=False, room=room_id)
+
+    users_in_room[room_id].remove(sid)
+    if len(users_in_room[room_id]) == 0:
+        users_in_room.pop(room_id)
+
+    rooms_sid.pop(sid)
+    names_sid.pop(sid)
+
+    print("\nusers: ", users_in_room, "\n")
+
+
+@socketio.on("data")
+def on_data(data):
+    sender_sid = data['sender_id']
+    target_sid = data['target_id']
+    if sender_sid != request.sid:
+        print("[Not supposed to happen!] request.sid and sender_id don't match!!!")
+
+    if data["type"] != "new-ice-candidate":
+        print('{} message from {} to {}'.format(
+            data["type"], sender_sid, target_sid))
+    socketio.emit('data', data, room=target_sid)
+
+
+@socketio.on("send_message")
+def on_send_message(data):
+    target_sid = data['target_id']
+    sender_id = data['sender_id']
+    message = data['message']
+    requests.post("http://localhost:5001/comments/upload", json={ "comment": message, "userId": sender_id })
+    socketio.emit('message', data, room=target_sid)
 
 
 if __name__ == '__main__':
